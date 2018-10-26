e35547ffb6de005e22bad90f4eda4332b7bf58c8
Alexander G <alexgarmatenko@gmail.com>
2018-10-26 13:59:59 +0300

    Update navigation issue between sections

--- project/application/src/app/modules/home/home.component.ts
+++ project/application/src/app/modules/home/home.component.ts
@@ -1,7 +1,7 @@
 import { OnInit, Inject, OnDestroy, Component, PLATFORM_ID, AfterViewInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { FormControl, Validators } from '@angular/forms';
-import { isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Language } from 'angular-l10n';
 
 import { SeoPropertiesService } from '../core/services/seo-properties/seo-properties.service';
@@ -14,11 +14,14 @@ import { SeoPropertiesService } from '../core/services/seo-properties/seo-proper
 export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
     @Language()
     lang: string;
-    inView: string;
+    currentNavItem: string;
     title = 'lazy-school';
-    lat = 50.002257;
-    lng = 36.250887;
-    animationDelay: any;
+    location: any = {
+        lat: 50.002257,
+        lng: 36.250887,
+    };
+    activateSectionAvailable = false;
+    activateSectionPromise: any;
 
     animation = {
         aboutUs: false,
@@ -32,14 +35,14 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
 
     lectoryFormModalName = new FormControl('', Validators.required);
     lectoryFormModalPhone = new FormControl('', Validators.required);
-    loginFormModalPassword = new FormControl('', Validators.required);
     conferenceFormModalName = new FormControl('', Validators.required);
+    conferenceFormModalPhone = new FormControl('', Validators.required);
 
     constructor(
-        private router: Router,
         private route: ActivatedRoute,
         private seoPropertiesService: SeoPropertiesService,
-        @Inject(PLATFORM_ID) private platformId: Object
+        @Inject(PLATFORM_ID) private platformId: Object,
+        @Inject(DOCUMENT) private document: any
     ) {}
 
     ngOnInit(): void {
@@ -56,46 +59,33 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-        this.route.fragment.subscribe(fragment => {
-            if (isPlatformBrowser(this.platformId)) {
-                clearTimeout(this.animationDelay);
-                this.animationDelay = setTimeout(() => {
-                    this.scrollToTheElement(fragment);
-                }, 300);
-            } else {
-                this.scrollToTheElement(fragment);
-            }
-        });
-    }
-
-    public scrollToTheElement(
-        fragment: string,
-        settings: any = { behavior: 'smooth', block: 'center', inline: 'center' }
-    ): void {
-        const element = document.querySelector(`#${fragment}`);
-        if (!!element) {
-            this.inView = fragment;
-            element.scrollIntoView(settings);
-            const width = this.getWindowWidth();
-            const activeMobileNav = document.querySelector('.show') ? true : false;
-            if (width < 992 && activeMobileNav) {
-                (document.querySelector('button.navbar-toggler') as HTMLElement).click();
-            }
-        }
+        const section = this.route.snapshot.fragment;
+        this.setCurrentNavItem(section);
+        this.scrollToSection(section);
+        setTimeout(() => (this.activateSectionAvailable = true), 300);
     }
 
     public setAnimationOnScroll(section: string): void {
         this.animation[section] = true;
     }
 
-    public scrollToFragment(fragment): void {
-        this.router.navigated = false;
-        this.router.navigate(['/'], { fragment: `${fragment}` });
+    public navigateToSection(section: string): void {
+        clearTimeout(this.activateSectionPromise);
+        this.activateSectionPromise = setTimeout(() => {
+            this.setCurrentNavItem(section);
+            this.updateUrlWithSection(section);
+            this.scrollToSection(section);
+        }, 300);
     }
 
-    public isElementInView(event: any = {}): void {
+    public activateSection(event: any = {}): void {
         if (!!event && event.data) {
-            this.scrollToFragment(event.data);
+            clearTimeout(this.activateSectionPromise);
+            this.activateSectionPromise = setTimeout(() => {
+                const section = event.data;
+                this.setCurrentNavItem(section);
+                this.updateUrlWithSection(section);
+            }, 300);
         }
     }
 
@@ -103,7 +93,40 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
         this.seoPropertiesService.setSeoProps(this.route.snapshot.data.seoProps);
     }
 
+    private scrollToSection(
+        section: string,
+        settings: any = { behavior: 'smooth', block: 'center', inline: 'center' }
+    ): void {
+        const element = this.document.querySelector(`#${section}`);
+        if (!!element) {
+            element.scrollIntoView(settings);
+        }
+    }
+
+    private updateUrlWithSection(section: string): void {
+        if (isPlatformBrowser(this.platformId)) {
+            window.history.replaceState({}, '', `/#${section}`);
+        }
+    }
+
+    private setCurrentNavItem(section: string): void {
+        this.currentNavItem = section;
+        this.closeNavOnMobile();
+    }
+
+    private closeNavOnMobile(): void {
+        const width = this.getWindowWidth();
+        const activeMobileNav = this.document.querySelector('.show') ? true : false;
+        if (width < 992 && activeMobileNav) {
+            (this.document.querySelector('button.navbar-toggler') as HTMLElement).click();
+        }
+    }
+
     private getWindowWidth(): number {
-        return window.innerWidth || 0;
+        if (isPlatformBrowser(this.platformId)) {
+            return window.innerWidth || 0;
+        }
+
+        return 0;
     }
 }
